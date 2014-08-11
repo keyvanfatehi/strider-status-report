d635a51a9227730ae7b4333e00e133ef3c55cb40
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-11 09:37:55 -0500

    setup globalroute
    keyvan@luchia.local:~/P/S/strider-status-report git:remotes/origin/test
    ❯❯❯ http get localhost:3000/ext/status-report/hello              ⏎ ✱
    HTTP/1.1 200 OK
    Connection: keep-alive
    Content-Length: 23
    Content-Type: application/json; charset=utf-8
    Date: Mon, 11 Aug 2014 14:37:36 GMT
    ETag: "625682680"
    Set-Cookie:
    connect.sid=s%3AfcLVVAYlYWLiF54UTDAZN9Lk.kytzdJ0ntL9MV6D0LRJjZtoDPN16xGSHZi2v%2FUdZgBw;
    Path=/; Expires=Thu, 11 Sep 2014 01:06:39
     GMT; HttpOnly
    Vary: Accept-Encoding
    X-Powered-By: Express
    
    [
        "Hello everybody"
    ]

--- webapp.js
+++ webapp.js
@@ -1,8 +1,9 @@
 // var getStatusReport = require('./getStatusReport')
 
 module.exports = {
+  // will be namespaced under /:org/:repo/api/status-report
   routes: function (app) {
-    app.get('/api/status', function (req, res) {
+    app.get('/hello', function (req, res) {
       return res.send(200,["Hello everybody"])
       // getStatusReport( function (err, statusReport) {
       //   if (err) {
@@ -11,6 +12,12 @@ module.exports = {
       //   }
       //   return res.send(200,statusReport)
       // })
+    })
+  },
+  // namespaced to /ext/status-report
+  globalRoutes: function (app, context) {
+    app.get('/hello', function (req, res) {
+      return res.send(200,["Hello everybody"])
     })
   }
 }
