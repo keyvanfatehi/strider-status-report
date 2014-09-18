842a3cd3c78358786b3565eb8f2ab5f94b2efb7c
Martin Frost <frost@ceri.se>
2014-09-18 13:21:55 +0200

    Rewrite getStatusReport.js using MapReduce

--- getStatusReport.js
+++ getStatusReport.js
@@ -3,37 +3,66 @@ var _ = require('lodash')
 
 module.exports = function (context) {
   var Job = context.models.Job
-    , Project = context.models.Project
     , items = []
 
   return new Promise(function (resolve, reject) {
-    Project.find({}, 'name branches.name').exec(function (err, projects) {
-      if (err) return reject(err);
-      Job.find({}, 'project ref.branch test_exitcode deploy_exitcode').exec(function (err, jobs) {
-        if (err) return reject(err);
-        _.each(projects, function (project) {
-          items.push({
-            project: project.name,
-            branches: _.map(project.branches, function (branch) {
-              var status = null;
-              var job = _.find(jobs, function (j) {
-                return j.ref.branch === branch.name
-              });
-              if (job) {
-                status = {
-                  test_exitcode: job.test_exitcode,
-                  deploy_exitcode: job.deploy_exitcode
-                }
-              }
-              return {
-                name: branch.name,
-                status: status
-              }
-            })
-          })
-        });
-        return resolve(items)
-      })
-    })
+    var command = {
+      map: map,
+      reduce: reduce,
+      finalize: finalize,
+      query: {},
+      sort: { 'finished': 1 },
+      out: { inline: 1 }
+    }
+
+    Job.mapReduce(command, function(err, results) {
+      if (err) { reject(err); }
+      return resolve(_.map(results,'value'));
+    });
   })
+}
+
+function map() {
+  emit(this.project, {
+    branch: this.ref.branch,
+    finished: this.finished,
+    data: {
+      test_exitcode: this.test_exitcode,
+      deploy_exitcode: this.deploy_exitcode,
+      plugin_data: this.plugin_data
+    }
+  });
+}
+
+function reduce(project, values) {
+  var branches = [],
+      jobs = {};
+
+  for(var i = 0; i < values.length; i++) {
+    if (! jobs[values[i].branch]) { jobs[values[i].branch] = []; }
+    jobs[values[i].branch].push(values[i]);
+  }
+
+  for (var key in jobs) {
+    print(jobs[key]);
+    branches.push({
+      name: key,
+      status: jobs[key].sort(function(x,y) {
+        return x.finished == y.finished;
+      })[jobs[key].length - 1].data
+    });
+  }
+
+  return {
+    project: project,
+    branches: branches
+  }
+}
+
+function finalize(key, reduced) {
+  var result = [];
+  for(var i = 0; i < reduced.length; i++) {
+    result.push(reduced[i].value);
+  }
+  return reduced;
 }
